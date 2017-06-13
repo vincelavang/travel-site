cae3bc60a3c8662083199621ff6d0be67359ba0a
Vince Lavang <vincelavang@gmail.com>
2017-06-13 14:37:05 -0700

    Added mock list of places to search from. #1

---
+++ main.js
@@ -0,0 +1,14 @@
+var places = [
+{country: "Aalborg, Denmark" , airport:"(AAL)"}
+{country:"Aalesund, Norway" , airpot:"(AES)"}
+{country:"Aarhus, Denmark - Bus service" , airport:"(ZID)"}
+{country:"Aarhus, Denmark - Tirstrup" , airport:"(AAR)"}
+{country:"Aasiaat, Greenland" , airport:"(JEG)"}
+{country:"Abadan, Iran" , airport:"(ABD)"}
+{country:"Abakan, Russia" , airport:"(ABA)"}
+{country:"Aberdeen, United Kingdom" , airport:"(ABZ)"}
+{country:"Abha, Saudi Arabia" , airport:"(AHB)"}
+{country:"Abidjan, Cote d'Ivoire" , airport:"(ABJ)"}
+{country:"Abu Dhabi, United Arab Emirates" , airport:"(AUH)"}
+{country:"Abu Simbel, Egypt" , airport:"(ABS)"}
+]
