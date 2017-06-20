e929d8bf7c4d8827c59e6e8b097e8b055cc13690
Vince Lavang <vincelavang@gmail.com>
2017-06-20 11:09:59 -0700

    Layout test. #2

--- main.js
+++ main.js
@@ -103,3 +103,15 @@ function renderPlace (place , days) {
   $data.textContent = days + " days"
   return $place
 }
+
+$(document).ready(function() {
+    $('#myCarousel').carousel({
+	interval: 10000
+	})
+
+    $('#myCarousel').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+    
+
+});
